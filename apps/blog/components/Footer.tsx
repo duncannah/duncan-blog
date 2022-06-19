e2a08799321f051bcc85163711183cc21d25a729
duncannah <38371914+duncannah@users.noreply.github.com>
2022-06-19 13:53:41 +0200

    Dynamic year in footer

--- apps/blog/components/Footer.tsx
+++ apps/blog/components/Footer.tsx
@@ -2,7 +2,7 @@ import { NextPage } from "next";
 import styles from "./Footer.module.scss";
 
 export const Footer: NextPage = () => {
-	return <footer className={styles.Footer}>{`© 2022`}</footer>;
+	return <footer className={styles.Footer}>{`© ${new Date().getFullYear()}`}</footer>;
 };
 
 export default Footer;
