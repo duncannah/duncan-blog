505b6dda3b3d8675af648ced83560504b3064a6a
duncannah <38371914+duncannah@users.noreply.github.com>
2022-06-23 15:52:59 +0200

    add favicon and other meta

--- apps/blog/pages/_app.tsx
+++ apps/blog/pages/_app.tsx
@@ -13,6 +13,11 @@ function CustomApp({ Component, pageProps, links }: AppProps & { links: Jsonify<
 			<Head>
 				<title>{process.env.NEXT_PUBLIC_BLOG_FULLNAME}</title>
 				<link rel={`stylesheet`} href={`https://rsms.me/inter/inter.css`} />
+				<link rel={`apple-touch-icon`} sizes={`180x180`} href={`/apple-touch-icon.png`} />
+				<link rel={`icon`} type={`image/png`} sizes={`32x32`} href={`/favicon-32x32.png`} />
+				<link rel={`icon`} type={`image/png`} sizes={`16x16`} href={`/favicon-16x16.png`} />
+				<link rel={`manifest`} href={`/site.webmanifest`} />
+				<meta name={`theme-color`} content={`#000`} />
 			</Head>
 			<main className={`app`}>
 				<Header links={links} />
