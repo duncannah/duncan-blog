2e1c936b428fef64c3c59809e818fcaa9efc7063
duncannah <38371914+duncannah@users.noreply.github.com>
2022-06-27 00:59:11 +0200

    don't rebuild during development

--- apps/cms/util/rebuild.ts
+++ apps/cms/util/rebuild.ts
@@ -3,6 +3,8 @@ import { exec } from "child_process";
 export const triggerRebuild = async (): Promise<void> => {
 	if (!process.env[`REBUILD_PATH`]) throw new Error(`REBUILD_PATH is not set`);
 
+	if (process.env[`NODE_ENV`] === `development`) return;
+
 	return new Promise((resolve, reject) => {
 		exec(`pnpm nx export`, { cwd: process.env[`REBUILD_PATH`] }, (error, stdout, stderr) => {
 			if (error) {
