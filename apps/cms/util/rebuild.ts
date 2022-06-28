b60a2b6eebc1061b261ea9f0fff0a43fde435e76
duncannah <38371914+duncannah@users.noreply.github.com>
2022-06-28 20:47:24 +0200

    rebuild queueing

--- apps/cms/util/rebuild.ts
+++ apps/cms/util/rebuild.ts
@@ -1,20 +1,41 @@
 import { exec } from "child_process";
 
-export const triggerRebuild = async (): Promise<void> => {
+let queue = 0;
+
+export const triggerRebuild = (): void => {
 	if (!process.env[`REBUILD_PATH`]) throw new Error(`REBUILD_PATH is not set`);
 
 	if (process.env[`NODE_ENV`] === `development`) return;
 
-	return new Promise((resolve, reject) => {
-		exec(`pnpm nx export`, { cwd: process.env[`REBUILD_PATH`] }, (error, stdout, stderr) => {
-			if (error) {
-				console.error(stderr);
-				return reject(error);
-			}
+	if (queue > 0) {
+		queue++;
+		return;
+	}
 
-			return resolve();
-		});
-	});
+	void rebuild();
+
+	async function rebuild() {
+		try {
+			await new Promise<void>((resolve, reject) => {
+				exec(`pnpm nx export`, { cwd: process.env[`REBUILD_PATH`] }, (error, _, stderr) => {
+					if (error) {
+						console.error(stderr);
+						return reject(error);
+					}
+
+					return resolve();
+				});
+			});
+		} catch (e) {
+			console.error(`Rebuild failed`);
+			console.error(e);
+		}
+
+		if (queue > 0) {
+			queue--;
+			await rebuild();
+		}
+	}
 };
 
 export default triggerRebuild;
