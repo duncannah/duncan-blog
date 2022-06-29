69a25ec7b04464d98b81a2db9dbd6114aaaf3bce
duncannah <38371914+duncannah@users.noreply.github.com>
2022-06-29 22:16:01 +0200

    rebuild status in cms homepage

--- apps/cms/pages/index.tsx
+++ apps/cms/pages/index.tsx
@@ -1,9 +1,13 @@
+import { GetServerSideProps } from "next";
 import Link from "next/link";
+import { lastRebuild } from "../util/rebuild";
+
 import styles from "./index.module.scss";
 
-export function Index() {
+export function Index({ lastRebuild }: { lastRebuild: string | null }) {
 	return (
 		<div className={styles.page}>
+			<h2>{`Navigation`}</h2>
 			<ul>
 				<li>
 					<Link href={`/edit-post`}>{`Create or edit a post`}</Link>
@@ -12,8 +16,26 @@ export function Index() {
 					<Link href={`/manage-website`}>{`Manage website`}</Link>
 				</li>
 			</ul>
+			<h2>{`Status`}</h2>
+			<p>
+				<pre className={`language-txt`}>
+					<code>
+						{`Last rebuild: `}
+						<strong>{lastRebuild ?? `[no rebuilds recently]`}</strong>
+					</code>
+				</pre>
+			</p>
 		</div>
 	);
 }
+
+// eslint-disable-next-line @typescript-eslint/require-await
+export const getServerSideProps: GetServerSideProps = async () => {
+	return {
+		props: {
+			lastRebuild: lastRebuild,
+		},
+	};
+};
 
 export default Index;
--- apps/cms/util/rebuild.ts
+++ apps/cms/util/rebuild.ts
@@ -2,6 +2,8 @@ import { exec } from "child_process";
 
 let queue = 0;
 
+export let lastRebuild: string | null = null;
+
 export const triggerRebuild = (): void => {
 	if (!process.env[`REBUILD_PATH`]) throw new Error(`REBUILD_PATH is not set`);
 
@@ -15,6 +17,8 @@ export const triggerRebuild = (): void => {
 	void rebuild();
 
 	async function rebuild() {
+		lastRebuild = `REBUILDING: ${new Date().toISOString()}`;
+
 		try {
 			await new Promise<void>((resolve, reject) => {
 				exec(`pnpm nx export`, { cwd: process.env[`REBUILD_PATH`] }, (error, _, stderr) => {
@@ -26,10 +30,13 @@ export const triggerRebuild = (): void => {
 					return resolve();
 				});
 			});
-		} catch (e) {
+		} catch (e: unknown) {
+			lastRebuild = `ERROR: ${new Date().toISOString()} - ${e instanceof Error ? e.message : Object.prototype.toString.call(e)}`;
 			console.error(`Rebuild failed`);
 			console.error(e);
 		}
+
+		lastRebuild = `DONE: ${new Date().toISOString()}`;
 
 		if (queue > 0) {
 			queue--;
