41ac4825495e9d6449c9f39b41e8e3374b671eb2
duncannah <38371914+duncannah@users.noreply.github.com>
2022-10-27 15:29:27 +0200

    feat: better fonts (close #20)

--- apps/blog/pages/_app.tsx
+++ apps/blog/pages/_app.tsx
@@ -12,7 +12,6 @@ function CustomApp({ Component, pageProps, links }: AppProps & { links: Jsonify<
 		<>
 			<Head>
 				<title>{process.env.NEXT_PUBLIC_BLOG_FULLNAME}</title>
-				<link rel={`stylesheet`} href={`https://rsms.me/inter/inter.css`} />
 				<link rel={`apple-touch-icon`} sizes={`180x180`} href={`/apple-touch-icon.png`} />
 				<link rel={`icon`} type={`image/png`} sizes={`32x32`} href={`/favicon-32x32.png`} />
 				<link rel={`icon`} type={`image/png`} sizes={`16x16`} href={`/favicon-16x16.png`} />
