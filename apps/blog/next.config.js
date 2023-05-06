6fd45f76aca6df47397e0240a79c582fe9d6f7d9
duncannah <38371914+duncannah@users.noreply.github.com>
2023-05-06 23:50:12 +0200

    chore: update nx

--- apps/blog/next.config.js
+++ apps/blog/next.config.js
@@ -1,10 +1,10 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const withNx = require(`@nrwl/next/plugins/with-nx`);
+const withNx = require(`@nx/next/plugins/with-nx`);
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const path = require(`path`);
 
 /**
- * @type {import('@nrwl/next/plugins/with-nx').WithNxOptions}
+ * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
  **/
 const nextConfig = {
 	nx: {
